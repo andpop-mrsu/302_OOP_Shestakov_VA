68e4c89eee3cf9fcfe1757897492dd2452a90dd3
Baklaniso <shestvyaches@gmail.com>
2025-02-06 19:51:30 +0300

    Task01

---
+++ Task01/hello.php
@@ -0,0 +1,6 @@
+<?php
+date_default_timezone_set('Europe/Moscow');
+$dateTime = date("d.m.Y H:i:s");
+$name = 'Шестаков Вячеслав';
+echo "Текущая дата и время: $dateTime";
+echo "\nФамилия и имя: $name\n";
